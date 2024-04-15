cb9e79ed3712d65c3ab2ba7f48d17808ebba8b38
Jennifer <87148355+JenniferSieber@users.noreply.github.com>
2024-04-15 10:54:04 -0600

    Update makeChange-brute-force.js

--- makeChange/makeChange-brute-force.js
+++ makeChange/makeChange-brute-force.js
@@ -1,13 +1,12 @@
 // BRUTE FORCE Algorithms
-// makeChange, that returns an integer that represents the least number of coins that add up to an amount where the amt is always divisible by 5.
+// makeChange, that returns an integer that represents the least number of coins that add up to the required amount of change.
 
 // coin values 5, 10, 25
 // amount = 40  output 25, 10, 5 === 3 coins
 // amount = 45  output 25, 10, 10 === 3 coins
 // amount = 70  output 25, 25, 10, 10 = 4 coins 
+// ****requires additional of edge cases
 
-
-// ****requires addition of edge cases
 // algo
 let recursionCounter = 0
 // let coins = [1, 10, 6]
@@ -27,4 +26,6 @@ const makeChange = (value, i) => {
   })
   return minCoins + 1
 }
+
+console.log('brute', makeChange([5,10,25], 40))
 console.log('brute', makeChange([1,6,10], 12))
